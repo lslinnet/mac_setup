5e5b1ce1d4d210b196f3f69b7ae7f8d0c73b1a44
Jurgen Verhasselt <sjugge@heretiksambrosia.net>
2012-11-11 20:14:18 +0100

    Update directory_structure.sh

--- directory_structure.sh
+++ directory_structure.sh
@@ -15,12 +15,20 @@ echo "Created ~/Workspace directory"
 ln -s ~/Cloud/Dropbox/Sjugge/Workspace/Credentials ~/Workspace/Credentials
 echo "Symlinked Credentials from Dropbox to ~/Workspace"
 
+# symlink Dropbox Sequel Pro folder to ~/Workspace
+ln -s Cloud/Dropbox/Sjugge/Workspace/Sequel_pro/ Workspace/Sequel_pro
+echo "Symlinked Sequel_pro from Dropbox to ~/Workspace
+
 # Setup Workspace
 cd ~/Workspace
 echo "Setting up the ~/Workspace"
 
 # Create VM directory
 mkdir VM
-echo "Created VM directory"
+echo "Created VM directory in Workspace"
+
+# Create Projects directory
+mkdir Projects
+echo "Projects directory created in Workspace"
 
 echo "finished running the directory strucure script"
