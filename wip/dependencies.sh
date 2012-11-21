a2985985709e7c7811f6c711de8b63eb3e9e2a9d
Jurgen Verhasselt <sjugge@heretiksambrosia.net>
2012-11-21 07:41:42 +0100

    work in progress

---
+++ wip/dependencies.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+### Dev setup script
+
+# Description: this script allows you to automate the setup of your Mac OS X box for Development compliant to Dynamo 2.0.
+# Updated: 2012/11/21
+# sjugge@heretiksambrosia.net
+
+## Dependencies
+
+if [ ! -f /Applications/Xcode.app ];
+then
+
+echo "no Xcode"
+fi
